356853719cb3d2f8d5b876825417566c61da39b3
Yury Pitsishin <yury@landmarksid.com>
2018-11-15 20:20:05 +1100

    Add startRemoveExpense

--- src/components/EditExpensePage.test.js
+++ src/components/EditExpensePage.test.js
@@ -3,16 +3,16 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from './EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
   editExpense = jest.fn();
-  removeExpense = jest.fn();
+  startRemoveExpense = jest.fn();
   history = { push: jest.fn() };
   wrapper = shallow(
     <EditExpensePage
       editExpense={editExpense}
-      removeExpense={removeExpense}
+      startRemoveExpense={startRemoveExpense}
       history={history}
       expense={expenses[1]}
     />)
@@ -31,6 +31,6 @@ test('should handle editExpense', () => {
 
 test('should handle removeExpense', () => {
   wrapper.find('button').simulate('click');
-  expect(removeExpense).toHaveBeenLastCalledWith({ id: expenses[1].id });
+  expect(startRemoveExpense).toHaveBeenLastCalledWith({ id: expenses[1].id });
   expect(history.push).toHaveBeenLastCalledWith('/');
 });
